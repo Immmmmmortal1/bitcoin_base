0c5d54a0af3cc5adbc768f52a19a6d21574bf5b5
Immmmmmortal1 <1347715389@qq.com>
2024-05-31 23:39:35 +0800

    Update btc_utils.dart

--- lib/src/utils/btc_utils.dart
+++ lib/src/utils/btc_utils.dart
@@ -6,4 +6,9 @@ class BtcUtils {
     dec = dec * BigRational(BigInt.from(10).pow(8));
     return dec.toBigInt();
   }
+  static String toBitcoin(BigInt satoshi) {
+    BigRational dec = BigRational(satoshi);
+    dec = dec / BigRational(BigInt.from(10).pow(8));
+    return dec.toStringAsFixed(8); // 确保有8位小数
+  }
 }
